52c155f5a0b77bcf68a8dce0aaaa4ea4714ddc04
Dan Engel <dengel29@gmail.com>
2014-08-05 21:23:58 -0400

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
   config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
